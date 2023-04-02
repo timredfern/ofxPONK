17e05dd65dc74ec9f4323c42bdb4705d53dc512d
Tim Redfern <tim@getdrop.com>
2023-04-02 20:18:50 +0100

    WIP

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -47,23 +47,26 @@ void testApp::draw(){
 
     vector <colourPolyline> lines;
 
-    for (int k=0;k<2;k++){
-        for (int j=0;j<2;j++){
+    for (int j=0;j<4;j++){
 
-            colourPolyline line;
+        ofPoint centre(
+            (ofGetWidth()/2)+(cos(ofGetElapsedTimef()+j)*200),
+            (ofGetHeight()/2)+(sin(ofGetElapsedTimef()+j)*200)
+            );
 
-            for (int i=0;i<NUM_POINTS+1;i++){
+        colourPolyline line;
+
+        for (int i=0;i<NUM_POINTS+1;i++){
                 line.addVertex(
-                    ((cos(float(i)*pfactor)+1)*CIRCLE_RADIUS)+(ofGetWidth()/2)+(-(CIRCLE_RADIUS+200)+(k*400)),
-                    ((sin(float(i)*pfactor)+1)*CIRCLE_RADIUS)+(ofGetHeight()/2)+(-(CIRCLE_RADIUS+200)+(j*400)),
+                    ((cos(float(i)*pfactor)+1)*CIRCLE_RADIUS)+centre.x,
+                    ((sin(float(i)*pfactor)+1)*CIRCLE_RADIUS)+centre.y,
                     (sin(float(i+(ofGetElapsedTimef()*30*j))*pfactor)+1)*128,
-                    (sin(float(i+(ofGetElapsedTimef()*57*k))*pfactor)+1)*128,
-                    (sin(float(i+(ofGetElapsedTimef()*101*(j+k)))*pfactor)+1)*128
+                    (sin(float(i+(ofGetElapsedTimef()*57*j))*pfactor)+1)*128,
+                    (sin(float(i+(ofGetElapsedTimef()*101*(j)))*pfactor)+1)*128
                     );
-            }
-            lines.push_back(line);
-            line.draw();
         }
+        lines.push_back(line);
+        line.draw();
     }
 
     int num = laser.draw(lines,255);
